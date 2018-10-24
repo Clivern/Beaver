4f036890664593128cf24ab541765ae291e611ae
Clivern <hello@clivern.com>
2018-10-24 16:48:58 +0200

    add mysql package

---
+++ internal/pkg/database/mysql.go
@@ -0,0 +1,12 @@
+// Copyright 2018 Clivern. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package database
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type MySQL struct {
+}
