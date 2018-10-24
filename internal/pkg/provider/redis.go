b09e6c60e864e26f069f08cd392907840571cdba
Clivern <hello@clivern.com>
2018-10-24 16:37:17 +0200

    Add redis & rabbitmq packages

---
+++ internal/pkg/provider/redis.go
@@ -0,0 +1,13 @@
+// Copyright 2018 Clivern. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package provider
+
+import (
+    "github.com/go-redis/redis"
+)
+
+type Redis struct {
+
+}
