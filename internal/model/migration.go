0b3d206aa488a2bfffa8a695ceb56dad02cbdcdf
Clivern <hello@clivern.com>
2018-11-07 22:57:37 +0100

    add migration model

--- internal/model/migration.go
+++ internal/model/migration.go
@@ -6,4 +6,27 @@ package model
 
 // Migration struct
 type Migration struct {
+	ID        int
+	Migration string
+	CreatedAt int
+}
+
+// Insert inserts records
+func (e *Migration) Insert() (error, bool) {
+	return nil, true
+}
+
+// Get gets records
+func (e *Migration) Get() (error, bool) {
+	return nil, true
+}
+
+// Count counts records
+func (e *Migration) Count() (error, bool) {
+	return nil, true
+}
+
+// Delete deletes records
+func (e *Migration) Delete() (error, bool) {
+	return nil, true
 }
