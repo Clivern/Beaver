359652dfb536ec1c37f1943b5d4748999f75b342
Clivern <hello@clivern.com>
2018-11-18 18:21:32 +0100

    add redis driver

--- internal/app/api/config.go
+++ internal/app/api/config.go
@@ -4,6 +4,31 @@
 
 package api
 
+// ConfigHash is the hash prefix
+const ConfigsHashPrefix string = "beaver.config"
+
 // Config struct
 type Config struct {
+	Key   string
+	Value string
+}
+
+// CreateConfig creates a config
+func (c *Config) CreateConfig(key string, value string) (string, error) {
+	return "", nil
+}
+
+// GetConfigByKey gets a config value with key
+func (c *Config) GetConfigByKey(key string) (string, error) {
+	return "", nil
+}
+
+// UpdateConfigByKey updates a config with key
+func (c *Config) UpdateConfigByKey(key string, value string) (bool, error) {
+	return true, nil
+}
+
+// DeleteConfigByKey deletes a config with key
+func (c *Config) DeleteConfigByKey(key string) (bool, error) {
+	return true, nil
 }
