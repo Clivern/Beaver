e8244a2420e4b5aa804ba97afbf2067fcd216bb4
Clivern <hello@clivern.com>
2018-11-23 10:29:17 +0100

    add correlation id to channel api & endpoint

--- internal/app/api/channel.go
+++ internal/app/api/channel.go
@@ -16,7 +16,8 @@ const ChannelsHashPrefix string = "beaver.channel"
 
 // Channel struct
 type Channel struct {
-	Driver *driver.Redis
+	Driver        *driver.Redis
+	CorrelationID string
 }
 
 // ChannelResult struct
@@ -53,7 +54,7 @@ func (c *Channel) Init() bool {
 
 	result, err := c.Driver.Connect()
 	if !result {
-		logger.Errorf("Error while connecting to redis: %s", err.Error())
+		logger.Errorf(`Error while connecting to redis: %s {"correlationId":"%s"}`, err.Error(), c.CorrelationID)
 		return false
 	}
 	return true
@@ -64,26 +65,26 @@ func (c *Channel) CreateChannel(channel ChannelResult) (bool, error) {
 	exists, err := c.Driver.HExists(ChannelsHashPrefix, channel.Name)
 
 	if err != nil {
-		logger.Errorf("Error while creating channel %s: %s", channel.Name, err.Error())
+		logger.Errorf(`Error while creating channel %s: %s {"correlationId":"%s"}`, channel.Name, err.Error(), c.CorrelationID)
 		return false, fmt.Errorf("Error while creating channel %s", channel.Name)
 	}
 
 	if exists {
-		logger.Warningf("Trying to create existent channel %s", channel.Name)
+		logger.Warningf(`Trying to create existent channel %s {"correlationId":"%s"}`, channel.Name, c.CorrelationID)
 		return false, fmt.Errorf("Trying to create existent channel %s", channel.Name)
 	}
 
 	result, err := channel.ConvertToJSON()
 
 	if err != nil {
-		logger.Errorf("Something wrong with channel %s data: %s", channel.Name, err.Error())
+		logger.Errorf(`Something wrong with channel %s data: %s {"correlationId":"%s"}`, channel.Name, err.Error(), c.CorrelationID)
 		return false, fmt.Errorf("Something wrong with channel %s data", channel.Name)
 	}
 
 	_, err = c.Driver.HSet(ChannelsHashPrefix, channel.Name, result)
 
 	if err != nil {
-		logger.Errorf("Error while creating channel %s: %s", channel.Name, err.Error())
+		logger.Errorf(`Error while creating channel %s: %s {"correlationId":"%s"}`, channel.Name, err.Error(), c.CorrelationID)
 		return false, fmt.Errorf("Error while creating channel %s", channel.Name)
 	}
 
@@ -97,26 +98,26 @@ func (c *Channel) GetChannelByName(name string) (ChannelResult, error) {
 	exists, err := c.Driver.HExists(ChannelsHashPrefix, name)
 
 	if err != nil {
-		logger.Errorf("Error while getting channel %s: %s", name, err.Error())
+		logger.Errorf(`Error while getting channel %s: %s {"correlationId":"%s"}`, name, err.Error(), c.CorrelationID)
 		return channelResult, fmt.Errorf("Error while getting channel %s", name)
 	}
 
 	if !exists {
-		logger.Warningf("Trying to get non existent channel %s", name)
+		logger.Warningf(`Trying to get non existent channel %s {"correlationId":"%s"}`, name, c.CorrelationID)
 		return channelResult, fmt.Errorf("Trying to get non existent channel %s", name)
 	}
 
 	value, err := c.Driver.HGet(ChannelsHashPrefix, name)
 
 	if err != nil {
-		logger.Errorf("Error while getting channel %s: %s", name, err.Error())
+		logger.Errorf(`Error while getting channel %s: %s {"correlationId":"%s"}`, name, err.Error(), c.CorrelationID)
 		return channelResult, fmt.Errorf("Error while getting channel %s", name)
 	}
 
 	_, err = channelResult.LoadFromJSON([]byte(value))
 
 	if err != nil {
-		logger.Errorf("Error while getting channel %s: %s", name, err.Error())
+		logger.Errorf(`Error while getting channel %s: %s {"correlationId":"%s"}`, name, err.Error(), c.CorrelationID)
 		return channelResult, fmt.Errorf("Error while getting channel %s", name)
 	}
 
@@ -128,26 +129,26 @@ func (c *Channel) UpdateChannelByName(channel ChannelResult) (bool, error) {
 	exists, err := c.Driver.HExists(ChannelsHashPrefix, channel.Name)
 
 	if err != nil {
-		logger.Errorf("Error while updating channel %s: %s", channel.Name, err.Error())
+		logger.Errorf(`Error while updating channel %s: %s {"correlationId":"%s"}`, channel.Name, err.Error(), c.CorrelationID)
 		return false, fmt.Errorf("Error while updating channel %s", channel.Name)
 	}
 
 	if !exists {
-		logger.Warningf("Trying to create non existent channel %s", channel.Name)
+		logger.Warningf(`Trying to create non existent channel %s {"correlationId":"%s"}`, channel.Name, c.CorrelationID)
 		return false, fmt.Errorf("Trying to create non existent channel %s", channel.Name)
 	}
 
 	result, err := channel.ConvertToJSON()
 
 	if err != nil {
-		logger.Errorf("Something wrong with channel %s data: %s", channel.Name, err.Error())
+		logger.Errorf(`Something wrong with channel %s data: %s {"correlationId":"%s"}`, channel.Name, err.Error(), c.CorrelationID)
 		return false, fmt.Errorf("Something wrong with channel %s data", channel.Name)
 	}
 
 	_, err = c.Driver.HSet(ChannelsHashPrefix, channel.Name, result)
 
 	if err != nil {
-		logger.Errorf("Error while updating channel %s: %s", channel.Name, err.Error())
+		logger.Errorf(`Error while updating channel %s: %s {"correlationId":"%s"}`, channel.Name, err.Error(), c.CorrelationID)
 		return false, fmt.Errorf("Error while updating channel %s", channel.Name)
 	}
 
@@ -159,12 +160,12 @@ func (c *Channel) DeleteChannelByName(name string) (bool, error) {
 	deleted, err := c.Driver.HDel(ChannelsHashPrefix, name)
 
 	if err != nil {
-		logger.Errorf("Error while deleting channel %s: %s", name, err.Error())
+		logger.Errorf(`Error while deleting channel %s: %s {"correlationId":"%s"}`, name, err.Error(), c.CorrelationID)
 		return false, fmt.Errorf("Error while deleting channel %s", name)
 	}
 
 	if deleted <= 0 {
-		logger.Warningf("Trying to delete non existent channel %s", name)
+		logger.Warningf(`Trying to delete non existent channel %s {"correlationId":"%s"}`, name, c.CorrelationID)
 		return false, fmt.Errorf("Trying to delete non existent channel %s", name)
 	}
 
