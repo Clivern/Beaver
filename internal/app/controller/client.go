9c40c08e35f068887059a53a5a5b8b88fe932c38
Clivern <hello@clivern.com>
2018-12-03 20:52:31 +0100

    subscribe & unsubscribe endpoints

--- internal/app/controller/client.go
+++ internal/app/controller/client.go
@@ -145,14 +145,102 @@ func DeleteClientByID(c *gin.Context) {
 
 // Unsubscribe controller
 func Unsubscribe(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"status": "ok",
-	})
+
+	var clientResult api.ClientResult
+	ID := c.Param("id")
+
+	client := api.Client{
+		CorrelationID: c.Request.Header.Get("X-Correlation-ID"),
+	}
+
+	rawBody, err := c.GetRawData()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "error",
+			"error":  "Invalid request",
+		})
+		return
+	}
+
+	ok, err := clientResult.LoadFromJSON(rawBody)
+
+	if !ok || err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "error",
+			"error":  "Invalid request",
+		})
+		return
+	}
+
+	if !client.Init() {
+		c.JSON(http.StatusServiceUnavailable, gin.H{
+			"status": "error",
+			"error":  "Internal server error",
+		})
+		return
+	}
+
+	ok, err = client.Unsubscribe(ID, clientResult.Channels)
+
+	if !ok || err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "error",
+			"error":  err.Error(),
+		})
+		return
+	}
+
+	c.Status(http.StatusOK)
 }
 
 // Subscribe controller
 func Subscribe(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"status": "ok",
-	})
+
+	var clientResult api.ClientResult
+	ID := c.Param("id")
+
+	client := api.Client{
+		CorrelationID: c.Request.Header.Get("X-Correlation-ID"),
+	}
+
+	rawBody, err := c.GetRawData()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "error",
+			"error":  "Invalid request",
+		})
+		return
+	}
+
+	ok, err := clientResult.LoadFromJSON(rawBody)
+
+	if !ok || err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "error",
+			"error":  "Invalid request",
+		})
+		return
+	}
+
+	if !client.Init() {
+		c.JSON(http.StatusServiceUnavailable, gin.H{
+			"status": "error",
+			"error":  "Internal server error",
+		})
+		return
+	}
+
+	ok, err = client.Subscribe(ID, clientResult.Channels)
+
+	if !ok || err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "error",
+			"error":  err.Error(),
+		})
+		return
+	}
+
+	c.Status(http.StatusOK)
 }
