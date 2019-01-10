b1d550a3b04f519c7977a07ed7a88dccb83d7c56
Clivern <hello@clivern.com>
2019-01-10 01:03:55 +0100

    Fix assets issue

--- internal/app/controller/home.go
+++ internal/app/controller/home.go
@@ -11,7 +11,8 @@ import (
 
 // Index controller
 func Index(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.tmpl", gin.H{
-		"title": "Beaver",
-	})
+	homeTpl := `<!doctype html><html> <head> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <title>Beaver</title> <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css"> <style> html, body { background-color: #fff; color: #636b6f; font-family: 'Nunito', sans-serif; font-weight: 200; height: 100vh; margin: 0; } .full-height { height: 100vh; } .flex-center { align-items: center; display: flex; justify-content: center; } .position-ref { position: relative; } .top-right { position: absolute; right: 10px; top: 18px; } .content { text-align: center; } .title { font-size: 30px; } .links > a { color: #636b6f; padding: 0 25px; font-size: 12px; font-weight: 600; letter-spacing: .1rem; text-decoration: none; text-transform: uppercase; } .m-b-md { margin-bottom: 30px; } </style> </head> <body> <div class="flex-center position-ref full-height"> <div class="content"> <div class="title"> Beaver </div> <p>A Real Time Messaging Server.</p> </div> </div> </body></html>`
+	c.Writer.WriteHeader(http.StatusOK)
+	c.Writer.Write([]byte(homeTpl))
+	return
 }
